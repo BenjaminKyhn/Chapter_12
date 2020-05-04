806c4bc497ba143f84baffc7d035f4e5a71e258e
Benjamin Kyhn <benkyhn@hotmail.com>
2020-05-04 17:29:37 +0200

    Added exercise 12.4

---
+++ src/Exercise_12_04/Exercise_12_04.java
@@ -0,0 +1,9 @@
+package Exercise_12_04;
+
+import java.util.Scanner;
+
+public class Exercise_12_04 {
+    public static void main(String[] args) {
+        Loan loan = new Loan(-0.60, 5, 330000);
+    }
+}
